79afe3cd702a8bb9e0b6fd0291aebf7bfecf9e44
Александр <@HOME-PC>
2023-04-20 21:12:00 +0500

    Templastes and static added

--- flaskr/db.py
+++ flaskr/db.py
@@ -17,10 +17,9 @@ def get_db():
 
 def close_db(e=None):
     db = g.pop('db', None)
-    
+
     if db is not None:
         db.close()
-
 
 def init_db():
     db = get_db()
@@ -33,7 +32,7 @@ def init_db():
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
-    click.echo("Initialized the database")
+    click.echo('Initialized the database.')
 
 
 def init_app(app):
